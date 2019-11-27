93de56f0d349275e7110987f3d3776c31ef87228
unknown <gguelber@yahoo.com.br>
2019-11-26 23:17:35 -0300

    Updates

---
+++ precache-manifest.8d5ab8a59f8fabb33539ea97f37cc1bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbf3c742ae2a68d7e84c70f654a911e",
+    "url": "/Heroes-Quiz-API/index.html"
+  },
+  {
+    "revision": "7eeb007958a97e9f510e",
+    "url": "/Heroes-Quiz-API/static/css/main.7fe5e623.chunk.css"
+  },
+  {
+    "revision": "77b21ea6a83be04fea65",
+    "url": "/Heroes-Quiz-API/static/js/2.8353ba06.chunk.js"
+  },
+  {
+    "revision": "7eeb007958a97e9f510e",
+    "url": "/Heroes-Quiz-API/static/js/main.2fa15765.chunk.js"
+  },
+  {
+    "revision": "987123283e393ad3546c",
+    "url": "/Heroes-Quiz-API/static/js/runtime-main.3507664f.js"
+  },
+  {
+    "revision": "d83897ef7e540fbf7eb1e50e063d2061",
+    "url": "/Heroes-Quiz-API/static/media/marvel-vs-dc.d83897ef.jpeg"
+  }
+]);
